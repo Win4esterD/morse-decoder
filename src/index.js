f9a23004c00a10eb9fbf40f68774f79d421619d4
Pavel Didenko <patriot2006_06@mail.ru>
2023-10-25 20:47:27 +0300

    feat: solve the task, finally, unbelivable!

--- src/index.js
+++ src/index.js
@@ -1,46 +1,81 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
+  const dictionary = {
+    10: ".",
+    11: "-",
+  };
+  const wordsDecoded = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    wordsDecoded.push(expr.slice(i, i + 10));
+  }
+
+  const lettersDecoded = wordsDecoded.map((wordCoded) => {
+    const word = [];
+
+    for (let i = 0; i < wordCoded.length; i += 2) {
+      if (wordCoded[i] === "*") {
+        word.push(" ");
+        break;
+      } else if (wordCoded[i] === "1") {
+        word.push(dictionary[wordCoded[i] + wordCoded[i + 1]]);
+      }
+    }
+
+    return word;
+  });
+
+  let result = "";
+
+  for (let letter of lettersDecoded) {
+    if (letter.join("") !== " ") {
+      result += MORSE_TABLE[letter.join("")];
+    } else {
+      result += letter.join("");
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
